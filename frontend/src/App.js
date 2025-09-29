336e9ad43d0af90111b63b12faac45cc5a440861
DeividRome <davidjonas1094@gmail.com>
2025-09-29 16:27:02 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ZoomPhonePresentation from './components/ZoomPhonePresentation';
 import './App.css';
@@ -9,6 +9,26 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ZoomPhonePresentation />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;*/
+
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PresentationApp from "./components/ZoomPhonePresentation";
+
+function App() {
+  return (
+    <div className="App">
+      {/* process.env.PUBLIC_URL contendrá '/edbpresentacion' gracias al campo "homepage" */}
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<PresentationApp />} />
         </Routes>
       </BrowserRouter>
     </div>
